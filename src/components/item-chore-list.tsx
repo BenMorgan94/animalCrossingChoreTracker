270dbdbb43b0e6c5bf3e840ebae38a707919a124
Ben Morgan <benmorgan@MacBook-Pro.local>
2020-04-30 14:05:25 +0100

    feat(app): create chore list

---
+++ src/components/item-chore-list.tsx
@@ -0,0 +1,18 @@
+import React, { FunctionComponent } from "react";
+
+import { Chore } from '../interfaces/chore-interface';
+import { ChoreListItem } from "./item-chore";
+
+
+interface Props {
+  chores: Chore[];
+  onDelete: (chore: Chore) => void;
+}
+
+export const ChoresList: FunctionComponent<Props> = ({ chores, onDelete }) => (
+  <ul>
+    {chores.map(chore => (
+      <ChoreListItem chore={chore} onDelete={onDelete} />
+    ))}
+  </ul>
+);
