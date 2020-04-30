4aa964ae26533ce444ed4017640dc6b4d41a6577
Ben Morgan <benmorgan@MacBook-Pro.local>
2020-04-30 14:06:42 +0100

    feat(app): create single chore list item

---
+++ src/components/item-chore.tsx
@@ -0,0 +1,31 @@
+import React, { FunctionComponent } from "react";
+import { IonGrid, IonCol, IonRow, IonIcon, IonList, } from "@ionic/react";
+
+import { Chore } from '../interfaces/chore-interface';
+
+
+interface Props {
+  chore: Chore;
+  onDelete: (chore: Chore) => void;
+}
+
+export const ChoreListItem: FunctionComponent<Props> = ({ chore, onDelete }) => {
+  const onClick = () => {
+    onDelete(chore);
+  };
+
+  return (
+    <IonList>
+      <IonGrid>
+        <IonRow>
+          <IonCol>
+            {chore.name}
+          </IonCol>
+          <IonCol>
+            <IonIcon onClick={onClick}/>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+    </IonList>
+  );
+};
