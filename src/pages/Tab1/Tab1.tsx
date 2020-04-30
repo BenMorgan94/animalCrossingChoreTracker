6e540c28dcdbf9fe598466fe96b8bb4febcec24f
Ben Morgan <benmorgan@MacBook-Pro.local>
2020-04-30 14:07:29 +0100

    feat(app): create inital tab for create chore list

--- src/pages/Tab1/Tab1.tsx
+++ src/pages/Tab1/Tab1.tsx
@@ -1,21 +1,108 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import './Tab1.css';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonFab, IonFabButton, IonButton, IonPopover, } from '@ionic/react';
 
-const Tab1: React.FC = () => {
-  return (
-    <IonPage>
-      <IonContent class="page-content">
-        <IonContent class="header-content">
-          <IonHeader class="header-container">
+import { Chore } from '../../interfaces/chore-interface';
+import { NewChoreForm } from "../../components/form-new-chore";
+import { ChoresList } from "../../components/item-chore-list";
+
+
+interface State {
+  newChore: Chore;
+  chores: Chore[];
+  showInputPopover: boolean;
+}
+
+export default class Tab1 extends React.Component<{}, State> {
+  state = {
+    newChore: {
+      id: 1,
+      name: ""
+    },
+    
+    chores: [],
+    showInputPopover: false,
+  };
+
+  render() {
+    return (
+      <>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle size="small"> Track your daily chores here! </IonTitle>
+          </IonToolbar>
+        </IonHeader>
+
+        <IonContent>
+          <IonFab horizontal="center" vertical="top" edge={true} slot="fixed">
+            <IonFabButton size="small" onClick={this.showPopover}>
+              +
+            </IonFabButton>
+          </IonFab>
+
+          <IonPopover 
+            backdropDismiss={true}
+            isOpen={this.state.showInputPopover}
+            onDidDismiss={this.hidePopover}
+          >
             <IonToolbar>
-              <IonTitle class="tab-title" size="large">Track your daily chores here!</IonTitle>
+              <h4 className="centerButton"> New To-Do: </h4>
             </IonToolbar>
-          </IonHeader>
-        </IonContent>
-      </IonContent>
-    </IonPage>
-  );
-};
+            
+            <NewChoreForm
+              chore={this.state.newChore}
+              onAdd={this.addChore}
+              onChange={this.handleChoreChange}
+            />
 
-export default Tab1;
+            <div className="centerButton">
+              <IonButton className="subMaxWidth" expand="block" onClick={this.hidePopover}> Close </IonButton>
+            </div> <br/>
+          </IonPopover>
+
+          <ChoresList chores={this.state.chores} onDelete={this.deleteChore} />
+        </IonContent>
+      </>
+    );
+  }
+
+  private showPopover = () => {
+    this.setState({
+      showInputPopover: true,
+    });
+  }
+
+  private hidePopover = () => {
+    this.setState({
+      showInputPopover: false,
+    });
+  }
+
+  private addChore = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    this.setState(previousState => ({
+      newChore: {
+        id: previousState.newChore.id + 1,
+        name: ""
+      },
+      chores: [...previousState.chores, previousState.newChore]
+    }));
+  };
+
+  private handleChoreChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      newChore: {
+        ...this.state.newChore,
+        name: event.target.value
+      }
+    });
+  };
+
+  private deleteChore = (taskToDelete: Chore) => {
+    this.setState(previousState => ({
+      chores: [
+        ...previousState.chores.filter(chore => chore.id !== taskToDelete.id)
+      ]
+    }));
+  };
+};
