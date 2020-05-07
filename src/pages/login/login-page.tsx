5a349ba3022c693cd4d9feb66b227da6d61e4cda
Ben Morgan <benmorgan@MacBook-Pro.local>
2020-05-07 17:53:26 +0100

    feat(app): start populating register and login pages

--- src/pages/login/login-page.tsx
+++ src/pages/login/login-page.tsx
@@ -1,17 +1,45 @@
-import React from "react";
-import { IonContent, IonHeader, IonTitle, IonToolbar } from "@ionic/react";
+import React, { useState } from "react";
+import "./login-page.css";
+import {
+  IonContent,
+  IonTitle,
+  IonToolbar,
+  IonPage,
+  IonInput,
+  IonButton,
+} from "@ionic/react";
+import { Link } from "react-router-dom";
 
-export default class LoginPage extends React.Component<{}> {
-  render() {
-    return (
-      <IonContent>
-        <IonHeader>
-          <IonToolbar>
-            {" "}
-            <IonTitle>Login</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+const Login: React.FC = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function loginUser() {
+    console.log(username, password);
+  };
+
+  return (
+    <IonPage id="main-content">
+      <IonToolbar>
+        {" "}
+        <IonTitle>Login</IonTitle>
+      </IonToolbar>
+      <IonContent scrollY={false}>
+        <IonInput
+          placeholder="Username"
+          onIonChange={(e: any) => setUsername(e?.target.value)}
+        />
+        <IonInput
+          placeholder="Password"
+          onIonChange={(e: any) => setPassword(e?.target.value)}
+        />
+        <IonButton expand="block" onClick={loginUser}>
+          Login
+        </IonButton>
+            <Link to="/register-page">Register for an account</Link>
       </IonContent>
-    );
-  }
-}
+    </IonPage>
+  );
+};
+
+export default Login;
--- src/pages/register/register-page.tsx
+++ src/pages/register/register-page.tsx
@@ -1,17 +1,50 @@
-import React from "react";
-import { IonContent, IonHeader, IonTitle, IonToolbar } from "@ionic/react";
+import React, { useState } from "react";
+import "./register-page.css";
+import {
+  IonContent,
+  IonTitle,
+  IonToolbar,
+  IonPage,
+  IonInput,
+  IonButton,
+} from "@ionic/react";
+import { Link } from "react-router-dom";
 
-export default class RegisterPage extends React.Component<{}> {
-  render() {
-    return (
-      <IonContent>
-        <IonHeader>
-          <IonToolbar>
-            {" "}
-            <IonTitle>Register</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-      </IonContent>
-    );
+const Register: React.FC = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [cPassword, setCPassword] = useState("");
+
+  function registerUser() {
+    console.log(username, password, cPassword);
   }
-}
+
+  return (
+    <IonPage id="main-content">
+      <IonToolbar>
+        {" "}
+        <IonTitle>Register</IonTitle>
+      </IonToolbar>
+      <IonContent scrollY={false}>
+        <IonInput
+          placeholder="Username"
+          onIonChange={(e: any) => setUsername(e?.target.value)}
+        />
+        <IonInput
+          placeholder="Password"
+          onIonChange={(e: any) => setPassword(e?.target.value)}
+        />
+        <IonInput
+          placeholder="Confirm Password"
+          onIonChange={(e: any) => setCPassword(e?.target.value)}
+        />
+        <IonButton expand="block" onClick={registerUser}>
+          Register
+        </IonButton>
+        <Link to="/login-page">Already have an account?</Link>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Register;
